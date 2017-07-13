c2c4a7f9e41e34d55118a67e424274bbf11a6133
Florian Tönjes <skatepainter@yahoo.de>
2017-07-13 18:15:34 +0200

    Secured thread and post creation to only be available to users who have signed in.

--- ee_forum_web/src/com/floriantoenjes/ee/forum/web/SignInFilter.java
+++ ee_forum_web/src/com/floriantoenjes/ee/forum/web/SignInFilter.java
@@ -1,11 +1,16 @@
 package com.floriantoenjes.ee.forum.web;
 
+import javax.inject.Inject;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = "/thread_form.xhtml", dispatcherTypes = {DispatcherType.REQUEST, DispatcherType.FORWARD})
+@WebFilter(urlPatterns = {"/thread_form.xhtml", "/post_form.xhtml"}, dispatcherTypes = {DispatcherType.REQUEST, DispatcherType.FORWARD})
 public class SignInFilter implements Filter {
+
+    @Inject
+    private SignInController signInController;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -14,8 +19,13 @@ public class SignInFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        ServletContext servletContext = servletRequest.getServletContext();
-        servletContext.log("LoggingFilter");
+
+        if (signInController.getUser() == null) {
+            HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+            httpServletResponse.setStatus(401);
+            servletRequest.getRequestDispatcher("unauthorized.xhtml").forward(servletRequest, servletResponse);
+        }
+
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
