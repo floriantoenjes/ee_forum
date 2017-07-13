2519de36637912769399ec401bc2c134c368a4d7
Florian Tönjes <skatepainter@yahoo.de>
2017-07-13 14:22:10 +0200

    Started to implement security filter.

---
+++ ee_forum_web/src/com/floriantoenjes/ee/forum/web/SigninFilter.java
@@ -0,0 +1,26 @@
+package com.floriantoenjes.ee.forum.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/thread_form.xhtml", dispatcherTypes = {DispatcherType.REQUEST, DispatcherType.FORWARD})
+public class SigninFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        ServletContext servletContext = servletRequest.getServletContext();
+        servletContext.log("LoggingFilter");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
