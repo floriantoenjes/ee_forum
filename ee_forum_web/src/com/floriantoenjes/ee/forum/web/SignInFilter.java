4a4df88d77a0a57d2767ce723c87085371661181
Florian Tönjes <skatepainter@yahoo.de>
2017-07-14 14:20:01 +0200

    Started implementing authorization for editing threads and posts.

--- ee_forum_web/src/com/floriantoenjes/ee/forum/web/SignInFilter.java
+++ ee_forum_web/src/com/floriantoenjes/ee/forum/web/SignInFilter.java
@@ -3,10 +3,11 @@ package com.floriantoenjes.ee.forum.web;
 import javax.inject.Inject;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/thread_form.xhtml", "/post_form.xhtml"}, dispatcherTypes = {DispatcherType.REQUEST, DispatcherType.FORWARD})
+@WebFilter(urlPatterns = {"/*"}, dispatcherTypes = {DispatcherType.REQUEST, DispatcherType.FORWARD})
 public class SignInFilter implements Filter {
 
     @Inject
@@ -19,11 +20,17 @@ public class SignInFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String path = httpServletRequest.getRequestURI().substring(httpServletRequest.getContextPath().length());
 
-        if (signInController.getUser() == null) {
+        if ((path.startsWith("/thread_form") || path.startsWith("/post_form"))
+                && signInController.getUser() == null) {
+
             HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
             httpServletResponse.setStatus(401);
-            servletRequest.getRequestDispatcher("unauthorized.xhtml").forward(servletRequest, servletResponse);
+            servletRequest.getRequestDispatcher("/unauthorized.xhtml").forward(servletRequest, servletResponse);
+        } else if (path.matches("^/board/\\d+/thread/\\d+/edit$")) {
+            servletRequest.getRequestDispatcher("/unauthorized.xhtml").forward(servletRequest, servletResponse);
         }
 
         filterChain.doFilter(servletRequest, servletResponse);
