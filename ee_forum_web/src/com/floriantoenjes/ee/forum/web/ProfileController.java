7b53c9b6c5344f5afa88c82a9446cec4cbc48ec5
Florian Tönjes <skatepainter@yahoo.de>
2017-07-21 11:47:50 +0200

    Started implementing ProfileController

--- ee_forum_web/src/com/floriantoenjes/ee/forum/web/ProfileController.java
+++ ee_forum_web/src/com/floriantoenjes/ee/forum/web/ProfileController.java
@@ -2,8 +2,23 @@ package com.floriantoenjes.ee.forum.web;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.servlet.http.Part;
 
 @Named
 @RequestScoped
 public class ProfileController {
+
+    private Part part;
+
+    public void saveProfile() {
+
+    }
+
+    public Part getPart() {
+        return part;
+    }
+
+    public void setPart(Part part) {
+        this.part = part;
+    }
 }
