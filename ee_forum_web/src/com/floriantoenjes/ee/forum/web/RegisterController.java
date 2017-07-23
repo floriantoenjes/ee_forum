bdc1742aa74cc621a1799cbe2f9811c0729210d7
Florian Tönjes <skatepainter@yahoo.de>
2017-07-23 11:45:31 +0200

    Added confirm password field

--- ee_forum_web/src/com/floriantoenjes/ee/forum/web/RegisterController.java
+++ ee_forum_web/src/com/floriantoenjes/ee/forum/web/RegisterController.java
@@ -6,6 +6,7 @@ import com.floriantoenjes.ee.forum.ejb.model.User;
 import javax.ejb.EJB;
 import javax.el.ELContext;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -13,6 +14,8 @@ import javax.inject.Named;
 @Named
 @RequestScoped
 public class RegisterController {
+
+    private String confirmPassword;
 
     @Inject
     private SignInController signInController;
@@ -24,6 +27,11 @@ public class RegisterController {
     private UserBean userBean;
 
     public String register() {
+        if (!user.getPassword().equals(confirmPassword)) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("the passwords have to match"));
+            return "/register.xhtml";
+        }
+
         user = userBean.register(user);
         signInController.setUser(user);
 
@@ -36,5 +44,13 @@ public class RegisterController {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
     }
 }
